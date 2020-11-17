45db8e5e6769fa605258230a8e15250b95b93c22
sanjay <sanapplegates@gmail.com>
2020-11-17 17:39:27 +0530

    updated

---
+++ NLP/Fake News/fake_news_detection/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project predicts whether a news is fake or real.',
+    author='sanjay',
+    license='MIT',
+)
