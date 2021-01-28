ba3a0ee09e926a285e671db8c0a5cdfe66f6ffa1
sanjay <sanapplegates@gmail.com>
2021-01-29 03:33:46 +0530

    updated

---
+++ web-scrapping/ietf_scrapper/ietf_scrapper/spiders/ietf.py
@@ -0,0 +1,14 @@
+import scrapy
+
+
+class IetfSpider(scrapy.Spider):
+    name = 'ietf'
+    allowed_domains = ['pythonscraping.com']
+    start_urls = ['http://pythonscraping.com/linkeidn/ietf.html']
+
+    def parse(self, response):
+        #title = response.css('span.title::text').get()
+        title = response.xpath('//span[@class="title"])/text()').get()
+        return {"title": title}
+    
+        #pass
